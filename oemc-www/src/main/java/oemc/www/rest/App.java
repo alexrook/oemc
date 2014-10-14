77842d64bd3c70920a54c7d103f2bd1a4c859ca8
Alex <AlexRook>
2014-10-14 17:37:52 +0300

    add project files

---
+++ oemc-www/src/main/java/oemc/www/rest/App.java
@@ -0,0 +1,12 @@
+package oemc.www.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author moroz
+ */
+@ApplicationPath("/rst")
+public class App extends Application {
+  
+}
